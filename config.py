319b0f9e6fbdb0dd44143ec3f7a57aa733de7612
Siva16sk12 <sk16sk12@gmail.com>
2026-03-14 14:51:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,41 +1,33 @@
 import os
 
-basedir = os.path.abspath(os.path.dirname(__file__))
+# Database connection
+SQLALCHEMY_DATABASE_URI = os.getenv(
+    "SQLAZURECONNSTR_DEFAULTCONNECTION",
+    "Driver={ODBC Driver 17 for SQL Server};"
+    "Server=PUT_YOUR_SQL_SERVER.database.windows.net;"
+    "Database=cms;"
+    "Uid=PUT_YOUR_SQL_USERNAME;"
+    "Pwd=PUT_YOUR_SQL_PASSWORD;"
+    "Encrypt=yes;"
+    "TrustServerCertificate=no;"
+    "Connection Timeout=30;"
+)
 
-class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret-key'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT') or 'ENTER_STORAGE_ACCOUNT_NAME'
-    BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY') or 'ENTER_BLOB_STORAGE_KEY'
-    BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER') or 'ENTER_IMAGES_CONTAINER_NAME'
+# Flask secret key
+SECRET_KEY = os.getenv("SECRET_KEY", "this_is_my_secret_key")
 
-    SQL_SERVER = os.environ.get('SQL_SERVER') or 'ENTER_SQL_SERVER_NAME.database.windows.net'
-    SQL_DATABASE = os.environ.get('SQL_DATABASE') or 'ENTER_SQL_DB_NAME'
-    SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or 'ENTER_SQL_SERVER_USERNAME'
-    SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or 'ENTER_SQL_SERVER_PASSWORD'
-    # Below URI may need some adjustments for driver version, based on your OS, if running locally
-    SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://' + SQL_USER_NAME + '@' + SQL_SERVER + ':' + SQL_PASSWORD + '@' + SQL_SERVER + ':1433/' + SQL_DATABASE  + '?driver=ODBC+Driver+17+for+SQL+Server'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    ### Info for MS Authentication ###
-    ### As adapted from: https://github.com/Azure-Samples/ms-identity-python-webapp ###
-    CLIENT_SECRET = "ENTER_CLIENT_SECRET_HERE"
-    # In your production app, Microsoft recommends you to use other ways to store your secret,
-    # such as KeyVault, or environment variable as described in Flask's documentation here:
-    # https://flask.palletsprojects.com/en/1.1.x/config/#configuring-from-environment-variables
-    # CLIENT_SECRET = os.getenv("CLIENT_SECRET")
-    # if not CLIENT_SECRET:
-    #     raise ValueError("Need to define CLIENT_SECRET environment variable")
+# Azure Blob Storage
+BLOB_ACCOUNT = os.getenv("BLOB_ACCOUNT", "PUT_YOUR_STORAGE_ACCOUNT_NAME")
+BLOB_STORAGE_KEY = os.getenv("BLOB_STORAGE_KEY", "PUT_YOUR_STORAGE_KEY")
+BLOB_CONTAINER = os.getenv("BLOB_CONTAINER", "images")
 
-    AUTHORITY = "https://login.microsoftonline.com/common"  # For multi-tenant app, else put tenant name
-    # AUTHORITY = "https://login.microsoftonline.com/Enter_the_Tenant_Name_Here"
 
-    CLIENT_ID = "ENTER_CLIENT_ID_HERE"
+# Microsoft Login (Azure Entra ID)
+AZURE_CLIENT_ID = "PUT_YOUR_CLIENT_ID"
+AZURE_CLIENT_SECRET = "PUT_YOUR_CLIENT_SECRET"
+AZURE_TENANT_ID = "PUT_YOUR_TENANT_ID"
 
-    REDIRECT_PATH = "/getAToken"  # Used to form an absolute URL; must match to app's redirect_uri set in AAD
-
-    # You can find the proper permission names from this document
-    # https://docs.microsoft.com/en-us/graph/permissions-reference
-    SCOPE = ["User.Read"] # Only need to read user profile for this app
-
-    SESSION_TYPE = "filesystem"  # Token cache will be stored in server-side session
+AZURE_REDIRECT_URI = "https://udacitycms-hsggc0fbb9cpfayv.centralus-01.azurewebsites.net/auth"

faaac35003eca03d8149a23a9e6a1c438f9f2e1a
Siva16sk12 <sk16sk12@gmail.com>
2026-03-14 15:37:40 +0530

    Update views.py

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -1,128 +1,148 @@
-"""
-Routes and views for the flask application.
-"""
-
-from datetime import datetime
-from flask import render_template, flash, redirect, request, session, url_for
-from werkzeug.urls import url_parse
-from config import Config
+from flask import render_template, redirect, url_for, request, flash
+from flask_login import login_user, logout_user, login_required
 from FlaskWebProject import app, db
-from FlaskWebProject.forms import LoginForm, PostForm
-from flask_login import current_user, login_user, logout_user, login_required
 from FlaskWebProject.models import User, Post
-import msal
-import uuid
+from FlaskWebProject.forms import LoginForm, PostForm
+import requests
 
-imageSourceUrl = 'https://'+ app.config['BLOB_ACCOUNT']  + '.blob.core.windows.net/' + app.config['BLOB_CONTAINER']  + '/'
 
 @app.route('/')
 @app.route('/home')
 @login_required
 def home():
-    user = User.query.filter_by(username=current_user.username).first_or_404()
     posts = Post.query.all()
-    return render_template(
-        'index.html',
-        title='Home Page',
-        posts=posts
-    )
+    return render_template("index.html", posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+
+    form = LoginForm()
+
+    if form.validate_on_submit():
+
+        user = User.query.filter_by(username=form.username.data).first()
+
+        if user and user.check_password(form.password.data):
+            login_user(user)
+            return redirect(url_for('home'))
+
+        flash("Invalid username or password")
+
+    return render_template("login.html", form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
 
 @app.route('/new_post', methods=['GET', 'POST'])
 @login_required
 def new_post():
-    form = PostForm(request.form)
+
+    form = PostForm()
+
     if form.validate_on_submit():
+
         post = Post()
-        post.save_changes(form, request.files['image_path'], current_user.id, new=True)
-        return redirect(url_for('home'))
-    return render_template(
-        'post.html',
-        title='Create Post',
-        imageSource=imageSourceUrl,
-        form=form
-    )
+
+        post.save_changes(
+            form,
+            request.files.get("image"),
+            1,
+            True
+        )
+
+        return redirect(url_for("home"))
+
+    return render_template("post.html", form=form)
 
 
-@app.route('/post/<int:id>', methods=['GET', 'POST'])
+@app.route('/post/<id>', methods=['GET', 'POST'])
 @login_required
 def post(id):
-    post = Post.query.get(int(id))
-    form = PostForm(formdata=request.form, obj=post)
+
+    post = Post.query.get(id)
+
+    form = PostForm(obj=post)
+
     if form.validate_on_submit():
-        post.save_changes(form, request.files['image_path'], current_user.id)
-        return redirect(url_for('home'))
-    return render_template(
-        'post.html',
-        title='Edit Post',
-        imageSource=imageSourceUrl,
-        form=form
+
+        post.save_changes(
+            form,
+            request.files.get("image"),
+            1
+        )
+
+        return redirect(url_for("home"))
+
+    return render_template("post.html", form=form)
+
+
+# ---------------- MICROSOFT LOGIN ----------------
+
+@app.route("/login_microsoft")
+def login_microsoft():
+
+    tenant = app.config["AZURE_TENANT_ID"]
+    client_id = app.config["AZURE_CLIENT_ID"]
+    redirect_uri = app.config["AZURE_REDIRECT_URI"]
+
+    auth_url = (
+        f"https://login.microsoftonline.com/{tenant}/oauth2/v2.0/authorize"
+        f"?client_id={client_id}"
+        f"&response_type=code"
+        f"&redirect_uri={redirect_uri}"
+        f"&response_mode=query"
+        f"&scope=User.Read"
+        f"&state=12345"
     )
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if current_user.is_authenticated:
-        return redirect(url_for('home'))
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user is None or not user.check_password(form.password.data):
-            flash('Invalid username or password')
-            return redirect(url_for('login'))
-        login_user(user, remember=form.remember_me.data)
-        next_page = request.args.get('next')
-        if not next_page or url_parse(next_page).netloc != '':
-            next_page = url_for('home')
-        return redirect(next_page)
-    session["state"] = str(uuid.uuid4())
-    auth_url = _build_auth_url(scopes=Config.SCOPE, state=session["state"])
-    return render_template('login.html', title='Sign In', form=form, auth_url=auth_url)
+    return redirect(auth_url)
 
-@app.route(Config.REDIRECT_PATH)  # Its absolute URL must match your app's redirect_uri set in AAD
-def authorized():
-    if request.args.get('state') != session.get("state"):
-        return redirect(url_for("home"))  # No-OP. Goes back to Index page
-    if "error" in request.args:  # Authentication/Authorization failure
-        return render_template("auth_error.html", result=request.args)
-    if request.args.get('code'):
-        cache = _load_cache()
-        # TODO: Acquire a token from a built msal app, along with the appropriate redirect URI
-        result = None
-        if "error" in result:
-            return render_template("auth_error.html", result=result)
-        session["user"] = result.get("id_token_claims")
-        # Note: In a real app, we'd use the 'name' property from session["user"] below
-        # Here, we'll use the admin username for anyone who is authenticated by MS
-        user = User.query.filter_by(username="admin").first()
-        login_user(user)
-        _save_cache(cache)
-    return redirect(url_for('home'))
 
-@app.route('/logout')
-def logout():
-    logout_user()
-    if session.get("user"): # Used MS Login
-        # Wipe out user and its token cache from session
-        session.clear()
-        # Also logout from your tenant's web session
-        return redirect(
-            Config.AUTHORITY + "/oauth2/v2.0/logout" +
-            "?post_logout_redirect_uri=" + url_for("login", _external=True))
+@app.route("/auth")
+def auth():
 
-    return redirect(url_for('login'))
+    code = request.args.get("code")
 
-def _load_cache():
-    # TODO: Load the cache from `msal`, if it exists
-    cache = None
-    return cache
+    tenant = app.config["AZURE_TENANT_ID"]
 
-def _save_cache(cache):
-    # TODO: Save the cache, if it has changed
-    pass
+    token_url = f"https://login.microsoftonline.com/{tenant}/oauth2/v2.0/token"
 
-def _build_msal_app(cache=None, authority=None):
-    # TODO: Return a ConfidentialClientApplication
-    return None
+    data = {
+        "client_id": app.config["AZURE_CLIENT_ID"],
+        "client_secret": app.config["AZURE_CLIENT_SECRET"],
+        "grant_type": "authorization_code",
+        "code": code,
+        "redirect_uri": app.config["AZURE_REDIRECT_URI"],
+        "scope": "User.Read"
+    }
 
-def _build_auth_url(authority=None, scopes=None, state=None):
-    # TODO: Return the full Auth Request URL with appropriate Redirect URI
-    return None
+    token = requests.post(token_url, data=data).json()
+
+    access_token = token["access_token"]
+
+    headers = {"Authorization": f"Bearer {access_token}"}
+
+    user_data = requests.get(
+        "https://graph.microsoft.com/v1.0/me",
+        headers=headers
+    ).json()
+
+    username = user_data["displayName"]
+
+    user = User.query.filter_by(username=username).first()
+
+    if not user:
+        user = User(username=username)
+        user.set_password("microsoft_login")
+        db.session.add(user)
+        db.session.commit()
+
+    login_user(user)
+
+    return redirect(url_for("home"))

aa08dfc96a3ed7f8bc29f0411e4ec4266b14d296
Siva16sk12 <sk16sk12@gmail.com>
2026-03-14 16:19:54 +0530

    Update models.py

--- FlaskWebProject/models.py
+++ FlaskWebProject/models.py
@@ -1,68 +1,48 @@
-from datetime import datetime
-from FlaskWebProject import app, db, login
-from werkzeug.security import generate_password_hash, check_password_hash
+from FlaskWebProject import db
 from flask_login import UserMixin
-from azure.storage.blob import BlockBlobService
-import string, random
-from werkzeug.utils import secure_filename
-from flask import flash
-
-blob_container = app.config['BLOB_CONTAINER']
-blob_service = BlockBlobService(account_name=app.config['BLOB_ACCOUNT'], account_key=app.config['BLOB_STORAGE_KEY'])
-
-def id_generator(size=32, chars=string.ascii_uppercase + string.digits):
-    return ''.join(random.choice(chars) for _ in range(size))
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(UserMixin, db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
-    password_hash = db.Column(db.String(128))
 
-    def __repr__(self):
-        return '<User {}>'.format(self.username)
+```
+id = db.Column(db.Integer, primary_key=True)
+username = db.Column(db.String(64), unique=True, nullable=False)
+password = db.Column(db.String(128), nullable=False)
 
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
+posts = db.relationship('Post', backref='author', lazy=True)
 
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
+def set_password(self, password):
+    self.password = generate_password_hash(password)
 
-@login.user_loader
-def load_user(id):
-    return User.query.get(int(id))
+def check_password(self, password):
+    return check_password_hash(self.password, password)
+```
 
 class Post(db.Model):
-    __tablename__ = 'posts'
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(150))
-    author = db.Column(db.String(75))
-    body = db.Column(db.String(800))
-    image_path = db.Column(db.String(100))
-    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
-    def __repr__(self):
-        return '<Post {}>'.format(self.body)
+```
+id = db.Column(db.Integer, primary_key=True)
 
-    def save_changes(self, form, file, userId, new=False):
-        self.title = form.title.data
-        self.author = form.author.data
-        self.body = form.body.data
-        self.user_id = userId
+title = db.Column(db.String(150))
+author = db.Column(db.String(100))
+content = db.Column(db.Text)
 
-        if file:
-            filename = secure_filename(file.filename);
-            fileextension = filename.rsplit('.',1)[1];
-            Randomfilename = id_generator();
-            filename = Randomfilename + '.' + fileextension;
-            try:
-                blob_service.create_blob_from_stream(blob_container, filename, file)
-                if(self.image_path):
-                    blob_service.delete_blob(blob_container, self.image_path)
-            except Exception:
-                flash(Exception)
-            self.image_path =  filename
-        if new:
-            db.session.add(self)
-        db.session.commit()
+image_path = db.Column(db.String(200))
+
+def save_changes(self, form, image, user_id, new=True):
+
+    self.title = form.title.data
+    self.author = form.author.data
+    self.content = form.content.data
+
+    if image:
+        filename = image.filename
+        image.save("FlaskWebProject/static/" + filename)
+        self.image_path = filename
+
+    if new:
+        db.session.add(self)
+
+    db.session.commit()
+```
+

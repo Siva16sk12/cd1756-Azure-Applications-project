2c098cee889ad117c4bb1ac7a99b26e077e6c95a
Siva16sk12 <sk16sk12@gmail.com>
2026-03-17 18:18:14 +0530

    Update views.py

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -1,18 +1,5 @@
-"""
-Routes for the Flask application.
-"""
-
 from FlaskWebProject import app
 
-
-# Home route
-@app.route("/")
+@app.route('/')
 def home():
     return "App is working 🎉"
-
-
-# Test route (optional but useful)
-@app.route("/test")
-def test():
-    return "Test route working ✅"
-

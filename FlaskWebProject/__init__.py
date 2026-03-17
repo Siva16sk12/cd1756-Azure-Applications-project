15077dedf033d3b0ca14d6b967c6e62d129a4176
Siva16sk12 <sk16sk12@gmail.com>
2026-03-17 18:18:41 +0530

    Update __init__.py

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -1,15 +1,5 @@
-"""
-The flask application package.
-"""
-
-import logging
 from flask import Flask
 
-# Create Flask app
 app = Flask(__name__)
 
-# Basic logging (safe for Azure)
-logging.basicConfig(level=logging.INFO)
-
-# Import routes (VERY IMPORTANT)
 import FlaskWebProject.views

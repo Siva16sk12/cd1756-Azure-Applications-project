28af0984d340f826c99d149e0faf56f6c712d09f
Siva16sk12 <sk16sk12@gmail.com>
2026-03-17 17:19:16 +0530

    Update views.py

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -1,26 +1,36 @@
-from FlaskWebProject import app
-from flask import render_template, redirect, url_for, request
+from FlaskWebProject import app, db
+from flask import render_template, redirect, url_for, request, flash
 from FlaskWebProject.models import Post
-from FlaskWebProject import db
+from flask_login import login_required, current_user
 
-# Home route
+
+# Home page
 @app.route('/')
 def home():
     posts = Post.query.all()
     return render_template('index.html', posts=posts)
 
 
-# Create new post
+# Create post
 @app.route('/create', methods=['GET', 'POST'])
+@login_required
 def create():
     if request.method == 'POST':
         title = request.form.get('title')
-        content = request.form.get('content')
+        author = request.form.get('author')
+        body = request.form.get('body')
 
-        new_post = Post(title=title, content=content)
-        db.session.add(new_post)
+        post = Post(
+            title=title,
+            author=author,
+            body=body,
+            user_id=current_user.id
+        )
+
+        db.session.add(post)
         db.session.commit()
 
+        flash("Post created successfully!")
         return redirect(url_for('home'))
 
     return render_template('create.html')
@@ -28,10 +38,17 @@ def create():
 
 # Delete post
 @app.route('/delete/<int:id>')
+@login_required
 def delete(id):
     post = Post.query.get_or_404(id)
+
+    if post.user_id != current_user.id:
+        flash("Not authorized!")
+        return redirect(url_for('home'))
+
     db.session.delete(post)
     db.session.commit()
 
+    flash("Post deleted!")
     return redirect(url_for('home'))
 

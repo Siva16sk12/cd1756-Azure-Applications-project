8b01d6c657a04b3b0d15c814501abb11c3fb10bc
Siva16sk12 <sk16sk12@gmail.com>
2026-03-17 16:40:23 +0530

    Update __init__.py

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -1,19 +1,5 @@
-"""
-The flask application package.
-"""
-import logging
-from flask import Flask
-from config import Config
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
-from flask_session import Session
-
-app = Flask(__name__)
-app.config.from_object(Config)
-# TODO: Add any logging levels and handlers with app.logger
-Session(app)
-db = SQLAlchemy(app)
-login = LoginManager(app)
-login.login_view = 'login'
-
-import FlaskWebProject.views
+Flask==2.2.5
+gunicorn==21.2.0
+Flask-SQLAlchemy==3.0.5
+Flask-Login==0.6.3
+Flask-Session==0.5.0

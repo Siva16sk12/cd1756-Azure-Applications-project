4f871902070ebff31259658bf4b965b42a3fac36
Siva16sk12 <sk16sk12@gmail.com>
2026-05-19 15:23:45 +0530

    Update __init__.py

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -9,6 +9,11 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(Config)
+app.config['WTF_CSRF_ENABLED'] = False
+
+# Force HTTPS for url_for when behind proxy
+from werkzeug.middleware.proxy_fix import ProxyFix
+app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
 
 # Set up logging
 logging.basicConfig(level=logging.INFO)

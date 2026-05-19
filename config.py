52105d508490488d2f8bbcf297ed18e02587f9d0
Siva16sk12 <sk16sk12@gmail.com>
2026-05-19 13:25:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,30 +3,30 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret-key'
-    
+
     BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT') or 'cmsstorage2'
     BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY') or 'ENTER_BLOB_STORAGE_KEY'
     BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER') or 'images'
     BLOB_CONNECTION_STRING = os.environ.get('BLOB_CONNECTION_STRING') or 'ENTER_CONNECTION_STRING'
-    
+
     SQL_SERVER = os.environ.get('SQL_SERVER') or 'cms1.database.windows.net'
     SQL_DATABASE = os.environ.get('SQL_DATABASE') or 'cms'
     SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or 'cmsadmin'
     SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or 'ENTER_YOUR_PASSWORD'
-    
-    # FIXED connection string format
+
+    # CORRECT connection string format
     SQLALCHEMY_DATABASE_URI = (
-        'mssql+pyodbc://' + 
-        (os.environ.get('SQL_USER_NAME') or 'cmsadmin') + 
-        '@' + 
-        (os.environ.get('SQL_SERVER') or 'cms1.database.windows.net') + 
-        ':' + 
-        (os.environ.get('SQL_PASSWORD') or 'ENTER_YOUR_PASSWORD') + 
-        '@' + 
-        (os.environ.get('SQL_SERVER') or 'cms1.database.windows.net') + 
-        ':1433/' + 
-        (os.environ.get('SQL_DATABASE') or 'cms') + 
-        '?driver=ODBC+Driver+17+for+SQL+Server'
+        'mssql+pyodbc://'
+        + os.environ.get('SQL_USER_NAME', 'cmsadmin')
+        + '%40'
+        + os.environ.get('SQL_SERVER', 'cms1.database.windows.net')
+        + ':'
+        + os.environ.get('SQL_PASSWORD', 'ENTER_YOUR_PASSWORD')
+        + '@'
+        + os.environ.get('SQL_SERVER', 'cms1.database.windows.net')
+        + ':1433/'
+        + os.environ.get('SQL_DATABASE', 'cms')
+        + '?driver=ODBC+Driver+17+for+SQL+Server'
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 

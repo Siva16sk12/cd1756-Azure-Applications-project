a77f4f7bd1f7ab2d35e893cec8e4cc6ec691d64f
Siva16sk12 <sk16sk12@gmail.com>
2026-05-19 15:07:41 +0530

    Update __init__.py

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -6,7 +6,6 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from flask_session import Session
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -18,7 +17,6 @@ streamHandler = logging.StreamHandler()
 streamHandler.setLevel(logging.INFO)
 app.logger.addHandler(streamHandler)
 
-Session(app)
 db = SQLAlchemy(app)
 login = LoginManager(app)
 login.login_view = 'login'
